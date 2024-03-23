d759c205d3b5cbb0dc7e51d7a8a94961eea27573
cs1488 <58979252+cs1488@users.noreply.github.com>
2024-03-23 16:08:42 +0100

    modified lemming

--- game_objects/lemming_spawner/lemming_spawner.gd
+++ game_objects/lemming_spawner/lemming_spawner.gd
@@ -9,9 +9,11 @@ var direction = [Globals.NW,Globals.NE,Globals.SW,Globals.SE]
 
 @export var lemming_population: int = 10
 @export var init_direction: Vector2 = Globals.NE
-@export var init_epoch: Globals.Epoch = Globals.Epoch.PAST
 
+var epoch: Globals.Epoch
 
+func _ready():
+	epoch = get_parent().epoch
 
 
 
@@ -20,7 +22,8 @@ func _on_timer_timeout():
 	if lemming_population:
 		var lemming_scene = LEMMING_SCENE.instantiate()
 		lemming_scene.direction = direction[randi_range(0,3)]
-		lemming_scene.own_epoch = rand.randi_range(-1,1)
+		lemming_scene.own_epoch = epoch
 		lemming_population-=1
-		add_sibling(lemming_scene)
 		lemming_scene.global_position = global_position
+		add_sibling(lemming_scene)
+		
