05d684fbd7fc526bd41b5a1b9f18e0bbdde8adff
cs1488 <58979252+cs1488@users.noreply.github.com>
2024-03-23 16:13:41 +0100

    .

--- game_objects/lemming_spawner/lemming_spawner.gd
+++ game_objects/lemming_spawner/lemming_spawner.gd
@@ -21,9 +21,8 @@ func _ready():
 func _on_timer_timeout():
 	if lemming_population:
 		var lemming_scene = LEMMING_SCENE.instantiate()
-		lemming_scene.direction = direction[randi_range(0,3)]
+		lemming_scene.direction = init_direction#direction[randi_range(0,3)]
 		lemming_scene.own_epoch = epoch
 		lemming_population-=1
+		get_parent().add_sibling(lemming_scene)
 		lemming_scene.global_position = global_position
-		add_sibling(lemming_scene)
-		
