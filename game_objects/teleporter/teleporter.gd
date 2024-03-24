7212ee3e4c904a37bc4f6e78a65db2c328ea3e66
cs1488 <58979252+cs1488@users.noreply.github.com>
2024-03-24 16:52:21 +0100

    probably fixed teleporter

--- game_objects/teleporter/teleporter.gd
+++ game_objects/teleporter/teleporter.gd
@@ -3,19 +3,22 @@ class_name Teleporter extends Area2D
 signal ported
 @export var partner: Teleporter 
 @export var cooldown: Array
+var tele_epoch: Globals.Epoch
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.body_entered.connect(self._on_body_enter)
 	partner.ported.connect(handle_cooldown_for_body)
+	tele_epoch = get_parent().epoch
 
 func _on_body_enter(body: Node2D):
-	if body is Lemming:
-		print("Teleporter detected a lemming")
-		print("Partner is ",partner.global_position)
-		if body not in cooldown:
-			ported.emit(body)
-			handle_cooldown_for_body(body)
-			body.global_position = partner.global_position
+	if body.own_epoch == tele_epoch:
+		if body is Lemming:
+			print("Teleporter detected a lemming")
+			print("Partner is ",partner.global_position)
+			if body not in cooldown:
+				ported.emit(body)
+				handle_cooldown_for_body(body)
+				body.global_position = partner.global_position
 		
 		
 
